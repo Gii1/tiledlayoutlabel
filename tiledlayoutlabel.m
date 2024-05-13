a01db723a20100de19654f62faf51c4d1bcd9dc5
Gii <28624123+Gii1@users.noreply.github.com>
2024-05-13 21:34:54 +0200

    Created function file

---
+++ tiledlayoutlabel.m
@@ -0,0 +1,8 @@
+function tiledlayoutlabel(fig, varargin)
+    % TILEDLAYOUTLABEL Label each subplot of a figure with a
+    % tiledlayout
+    %   TILEDLAYOUTLABEL labels each subplot of the current figure using
+    %   letters in alphabetical order.
+    %   TILEDLAYOUTLABEL(labelset) specifies the set to use for labeling. 
+    %   TILEDLAYOUTLABEL("custom", customset) specifies a custom set.
+    %   TILEDLAYOUTLABEL(fig, __) uses the target figure.
